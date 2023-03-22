2070bc356e0b1354ee3718d1719eeca468a37f5e
Jason Arihi <93453124+viiszc@users.noreply.github.com>
2023-03-22 09:13:10 +0000

    #1 Refactor UI for Home & About #2 Fix URL issues

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,7 +4,6 @@ export default function About() {
 	return (
 		<>
 			<Navbar title={'About'} name={'Home'} link={'/'} />
-
 			<h1 className="flex font-semibold text-2xl py-5 px-5">
 				This page is still W.I.P
 			</h1>
